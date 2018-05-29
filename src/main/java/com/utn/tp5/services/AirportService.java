1647de60fd0792157a8c15995b701b7aee082128
rpereyra <bemonee@gmail.com>
2018-05-29 20:26:52 -0300

    Se agrega service a AirportService

--- src/main/java/com/utn/tp5/services/AirportService.java
+++ src/main/java/com/utn/tp5/services/AirportService.java
@@ -3,12 +3,14 @@ package com.utn.tp5.services;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.utn.tp5.models.Airport;
 import com.utn.tp5.repositories.AirportRepository;
 
 import lombok.NoArgsConstructor;
 
+@Service
 @NoArgsConstructor
 public class AirportService {
 	@Autowired
